b7d078ea229d4b717e767eb16fd3d603a81f6e94
Mamta Dhaker <mamta.dhaker28@gmail.com>
2017-11-16 12:33:04 +0530

    token

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -2,4 +2,4 @@ export const ttsConfig = { lang: 'en-US', pitch: 1, rate: 1 };
 
 export const sttConfig = { lang: 'en-US', continuous: true, interimResults: true, maxAlternatives: 1 };
 
-export const accessToken = '80b5717cf56448bdadbdf1e24d49e421';
+export const accessToken = 'b81e48c28d6c49729b3458373533a908';
